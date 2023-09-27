8c65f23ca099c32c8f99855f10a288a4b3fe8823
dendywool <zaharkhachaturyan@gmail.com>
2023-09-27 23:40:56 +0200

    second

--- src/components/functions/numbers.js
+++ src/components/functions/numbers.js
@@ -1,15 +1,4 @@
 const numbersArray = [3, 7, 18, 25, 32, 0, 24, 12, 8, 33, 29];
-// const startArray = [];
-// for(let i = 1; i<37; i++) {
-//     startArray.push(i)
-// }
-// function shuffleArray(array) {
-//     for (let i = array.length - 1; i > 0; i--) {
-//       const j = Math.floor(Math.random() * (i + 1));
-//       [array[i], array[j]] = [array[j], array[i]];
-//     }
-// }
-// shuffleArray(startArray)
 const resultArray = [32, 15, 19, 4, 21, 2, 25, 17, 34, 6, 27, 13, 36, 11, 30, 8, 23, 10, 5, 24, 16, 33, 1, 20, 14, 13, 9, 22, 18, 29, 7, 28, 12, 35, 3, 26, 0]
 
 console.log(numbersArray, resultArray.length)
